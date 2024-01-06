8db38ba528b0965b22c8961b89d9543b5d070e64
SpaghettiCodes <chuashixiang@gmail.com>
2024-01-06 21:42:18 +0800

    mariadb very scuffed but done

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,5 +1,21 @@
 #! /bin/bash
 
-echo "MARIADB STATUS ---"
-systemctl status mariadb
+# dear god someone please tell me what is wrong with mariadb
+service mariadb start
 
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+FLUSH PRIVILEGES;
+" > data.sql
+
+cat data.sql
+
+mariadb < data.sql
+
+# kill off the started mariadb
+service mariadb stop
+ 
+
+mysqld_safe
+# temp because what the fuck am i doing
+# tail -f
