75760c6a09703501ca91a846be73fa66b6a2974c
SpaghettiCodes <chuashixiang@gmail.com>
2024-01-06 23:13:48 +0800

    attempt to fix funny mariadb

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,21 +1,25 @@
 #! /bin/bash
 
 # dear god someone please tell me what is wrong with mariadb
-service mariadb start
+# service mariadb start
 
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+# 1. changes the root password
+# 2. creates the wordpress database if it does not exist
+# 3. creates a new user for the wordpress database
+
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
 GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
 FLUSH PRIVILEGES;
 " > data.sql
 
-cat data.sql
-
-mariadb < data.sql
+mysql < data.sql
 
 # kill off the started mariadb
-service mariadb stop
- 
+# service mariadb stop
 
+# activate the daemon verion of mariadb
 mysqld_safe
+
 # temp because what the fuck am i doing
 # tail -f
