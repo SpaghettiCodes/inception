4bcd0d68361bceca9a5e117a4d2af78a0e124536
SpaghettiCodes <chuashixiang@gmail.com>
2024-01-14 22:09:00 +0800

    vm blew up on my face

--- srcs/requirements/bonus/ftp/tools/init.sh
+++ srcs/requirements/bonus/ftp/tools/init.sh
@@ -1,3 +1,13 @@
 #! /bin/bash
 
+# creating user
+echo "Creating FTP User ..."
+adduser "$FTP_USR_LOGIN"
+usermod -a -G www-data "$FTP_USR_LOGIN"
+echo "$FTP_USR_LOGIN:$FTP_USR_PASS" | chpasswd
+echo "Created"
 
+echo "FTP Server is now running"
+exec vsftpd
+
+# tail -f
