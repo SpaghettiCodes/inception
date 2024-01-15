84e90a8728b1ec3b7494c4fc81db13843e5a8e15
cshi-xia <cshi-xia@cshi-xia.cshi-xia.42.fr>
2024-01-15 21:18:07 +0800

    ftp is basically done still seg fault, dont know why
    gonna do adminer now

--- srcs/requirements/bonus/ftp/tools/init.sh
+++ srcs/requirements/bonus/ftp/tools/init.sh
@@ -3,7 +3,7 @@
 ssldir=/etc/ssl/certs/ssl-cert-snakeoil.pem
 keydir=/etc/ssl/private/ssl-cert-snakeoil.key
 
-# oh boy its time to create a ssl certificate againnn
+# oh boy its time to create a ssl certificate again
 echo "Creating a SSL Certificate"
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout "$keydir" -out "$ssldir" -subj"/C=$country/ST=$state/L=$city/O=$org/OU=$position/CN=$domain_name" 2> /dev/null
 echo "Created!"
@@ -13,6 +13,8 @@ echo "Creating FTP User ..."
 useradd -m "$FTP_USR_LOGIN"
 usermod -a -G www-data "$FTP_USR_LOGIN"
 echo "$FTP_USR_LOGIN:$FTP_USR_PASS" | chpasswd
+
+echo "$FTP_USR_LOGIN" >> /etc/vsftpd.chroot_list
 echo "Created!"
 
 echo "FTP Server is now running"
