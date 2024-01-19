8c68e0299a99acef5119c312c1f0c86ef5df7783
cshi-xia <cshi-xia@cshi-xia.cshi-xia.42.fr>
2024-01-19 12:25:08 +0800

    changing to gitea

---
+++ srcs/requirements/bonus/gitea/tools/init.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# creates directory structure and sets permissions
+mkdir -p /var/lib/gitea/{custom,data,indexers,public,log}
+chown git: /var/lib/gitea/{data,indexers,log}
+chmod 750 /var/lib/gitea/{data,indexers,log}
+mkdir /etc/gitea
+chown root:git /etc/gitea
+chmod 770 /etc/gitea
+
+exec tail -f
